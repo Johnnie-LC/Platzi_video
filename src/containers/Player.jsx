97c93d5aaabb21350d08a283487cdffabed29a48
Johnnie Lopez <johesloc@gmail.com>
2021-07-27 18:54:37 -0500

    add event a button in player.jsx and add a publicPath in webpack

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import '../assets/styles/components/Player.scss'
 
-const Player = () => {
+// to back a before pages, we can use the props.history.goBack()
+
+const Player = (props) => {
+  const { history } = propss
+  //the id comes from the route
+  const { id } = props.match.params
+
   return (
     <div className="Player">
       <video controls autoPlay>
@@ -9,7 +15,9 @@ const Player = () => {
       </video>
 
       <div className="Player-back">
-        <button type="button">Regresar</button>
+        <button type="button" onClick={() => history.goBack()}>
+          Regresar
+        </button>
       </div>
     </div>
   )
