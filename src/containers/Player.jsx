704dec49433489432fd5b18e6d6ad1cd9e591ab1
Johnnie Lopez <johesloc@gmail.com>
2021-07-27 22:16:12 -0500

    connect redux with the Player.js to get the video and id that it is  playing

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/styles/components/Player.scss'
-
+import { connect } from 'react-redux'
+import { getVideoSource } from '../actions'
+import { Redirect } from 'react-router-dom'
 // to back a before pages, we can use the props.history.goBack()
 
 const Player = (props) => {
-  const { history } = propss
+  const { history, playing, getVideoSource } = props
   //the id comes from the route
   const { id } = props.match.params
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    getVideoSource(id)
+    setLoading(false)
+  }, [])
 
-  return (
+  if (loading) {
+    return <h1>Cargando Video...</h1>
+  }
+  // validar la informacion, si hay un video o no
+  const hasPlaying = Object.keys(playing).length > 0
+
+  return hasPlaying ? (
     <div className="Player">
       <video controls autoPlay>
-        <source src="" type="video/mp4" />
+        <source src={playing.source} type="video/mp4" />
       </video>
 
       <div className="Player-back">
@@ -20,7 +33,13 @@ const Player = (props) => {
         </button>
       </div>
     </div>
+  ) : (
+    <Redirect to="/404/" />
   )
 }
 
-export default Player
+const mapStateToProps = ({ playing }) => ({ playing })
+const mapDispatchToProps = {
+  getVideoSource,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Player)
